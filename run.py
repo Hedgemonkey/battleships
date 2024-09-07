56b740e9b344b1e11fa31e41eae0fe5d61e23afe
Hedgemonkey <89958029+Hedgemonkey@users.noreply.github.com>
2024-09-07 04:48:14 +0000

    Update: Add functions to populate the boards with ships and to display both boards with correct alignment

--- run.py
+++ run.py
@@ -59,3 +59,47 @@ def create_boards(size):
     computer_board = [['O' for _ in range(size)] for _ in range(size)]
     return player_board, computer_board
     
+# Function to place ships randomly
+def place_ships(board, num_ships):
+    ship_locations = []
+    for _ in range(num_ships):
+        while True:
+            row = random.randint(0, len(board) - 1)
+            col = random.randint(0, len(board[0]) - 1)
+            if board[row][col] == 'O':
+                board[row][col] = 'S'
+                ship_locations.append((row, col))
+                break
+    return ship_locations
+
+# Function to display both boards side by side with colors and spacing
+def display_boards(player_board, computer_board):
+    board_size = len(player_board)
+    header_spacing = board_size
+    if board_size == 9:
+        header_spacing -= 1
+    elif board_size == 8:
+        header_spacing -= 2
+    elif board_size == 7:
+        header_spacing -= 3
+    elif board_size == 6:
+        header_spacing -= 4
+    elif board_size == 5:
+        header_spacing -= 5
+    print("\nPlayer Board  " + " "*header_spacing + " |  Computer Board")
+    header_length = len("Player Board") + len("Computer Board") + 3  # 3 for spaces
+    spacing = 0
+    if board_size < 10:
+        spacing += 1
+    print(spacing)
+    print("   " + " ".join([str(i) for i in range(1, board_size + 1)]) + " "*spacing + "  |" + "    " + " ".join([str(i) for i in range(1, board_size + 1)]) + "  ")
+    letters = list(string.ascii_uppercase[:board_size])
+    for i in range(board_size):
+        player_row = ' '.join(player_board[i])
+        computer_row = ' '.join(computer_board[i])
+        print(f"{letters[i]:<2} {player_row:<{len(player_row) + 2}} | {letters[i]:<2} {computer_row:<{len(player_row) + 2}}")
+
+player_board, computer_board = create_boards(5)
+player_ships = place_ships(player_board, 5)  # 5 ships of length 1
+computer_ships = place_ships(computer_board, 5)
+display_boards(player_board, computer_board)
